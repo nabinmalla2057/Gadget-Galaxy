bff9502c9c2a333230dfe8988e568c576906d935
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-21 21:30:22 +0545

    hash complete

--- server/modules/users/user.controller.js
+++ server/modules/users/user.controller.js
@@ -1,7 +1,12 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const Model = require("./user.model");
 
-const create = (payload) => {
-  return Model.create(payload);
+const create = async (payload) => {
+  const { password, ...rest } = payload;
+  rest.password = await bcrypt.hash(password, saltRounds);
+
+  return Model.create(rest);
 };
 const login = (email, password) => {};
 
