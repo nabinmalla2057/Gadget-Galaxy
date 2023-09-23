44104fc62b73f377bcbd782224f5626c5b268618
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-23 23:23:08 +0545

    OTP generate and verification

--- server/modules/auth/auth.controller.js
+++ server/modules/auth/auth.controller.js
@@ -1,43 +1,66 @@
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+const { generateOTP, verifyOTP } = require("../../utils/otp");
 const authModel = require("./auth.model");
 const userModel = require("../users/user.model");
+const { mailer } = require("../../services/mail");
+const { generateJWT } = require("../../utils/jwt");
 
 const create = async (payload) => {
   const { password, ...rest } = payload;
   rest.password = await bcrypt.hash(password, +process.env.SALT_ROUNDS);
-  await authModel.create({ email: payload.email, token: "123456" });
-  return userModel.create(rest);
+  await userModel.create(rest);
+  const token = generateOTP();
+  await authModel.create({ email: payload.email, token });
+  // send token to email
+  const info = await mailer(payload.email, token);
+  return info;
 };
+
 const login = async (email, password) => {
-  const user = await userModel.findOne({ email });
-  if (!user) throw new Error("User not Found");
-  if (!user.isActive)
-    throw new Error("User is not approved.Please contact Admin");
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) throw new Error("User not found");
+  if (!user.isActive) throw new Error("User is blocked. Please contact Admin");
   if (!user.isEmailVerified)
-    throw new Error("Email isnot verified. Verify your email");
+    throw new Error("Email is not verified. Verify email to get started");
   const isValid = await bcrypt.compare(password, user.password);
   if (!isValid) throw new Error("Email or Password is invalid");
-  return true;
+  // JWT TOKEN GENERATION
+  const payload = { email: user?.email, roles: user?.roles ?? [] };
+  const token = generateJWT(payload);
+  return { token };
 };
 
 const verifyEmail = async (emailP, tokenP) => {
   // check email exist
   const user = await authModel.findOne({ email: emailP });
   if (!user) throw new Error("User not found");
-
+  // check token Validity
+  const isValidToken = verifyOTP(tokenP);
+  if (!isValidToken) throw new Error("Token invalid");
   // token compare
   const isValid = user?.token === tokenP;
-  if (!isValid) throw new Error("token invalid");
-
-  //user isEmailVerified true
-  await user.Model.findOneAndUpdate(
-    { email },
+  if (!isValid) throw new Error("Token expired");
+  // user isEmailVerified true
+  await userModel.findOneAndUpdate(
+    { email: emailP },
     { isEmailVerified: true },
     { new: true }
   );
-  // authModel email doc deleteS
+  // authModel email doc delete
   await authModel.deleteOne({ email: emailP });
   return true;
-  module.exports = { create, login, verifyEmail };
 };
+
+const regenerate = async (email) => {
+  // check email exist
+  const user = await authModel.findOne({ email });
+  if (!user) throw new Error("User not found");
+  const token = generateOTP();
+  await authModel.findOneAndUpdate({ email }, { token }, { new: true });
+  // send token to email
+  await mailer(email, token);
+  return true;
+};
+
+module.exports = { create, login, verifyEmail, regenerate };
--- server/modules/auth/auth.route.js
+++ server/modules/auth/auth.route.js
@@ -13,7 +13,7 @@ router.post("/register", async (req, res, next) => {
 router.get("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password) throw new Error("Email or Password is missing");
+    if (!email || !password) throw new Error("Email or password is missing");
     const result = await Controller.login(email, password);
     res.json({ data: result, msg: "success" });
   } catch (e) {
@@ -23,9 +23,20 @@ router.get("/login", async (req, res, next) => {
 
 router.get("/verify", async (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    if (!email || !password) throw new Error("Email or Token is missing");
-    const result = await Controller.login(email, token);
+    const { email, token } = req.body;
+    if (!email || !token) throw new Error("Email or Token is missing");
+    const result = await Controller.verifyEmail(email, token);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/regenerate", async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) throw new Error("Email is missing");
+    const result = await Controller.regenerate(email);
     res.json({ data: result, msg: "success" });
   } catch (e) {
     next(e);
