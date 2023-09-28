fc2540dfe98f2aa266a9ed8dcb5a478a3fb96ff3
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-28 23:43:18 +0545

    category controller creayed

---
+++ server/modules/categories/category.controller.js
@@ -0,0 +1,10 @@
+const Model = require("./category.model");
+
+const create = (payload) => {};
+const list = (limit, page, search) => {};
+
+const getById = (id) => {};
+
+const updateById = (id, payload) => {};
+
+const remove = (id) => {};
