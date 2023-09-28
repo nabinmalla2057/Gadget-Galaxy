fc09fe18e144359554ef3ca5d984afb8a6655114
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-28 23:36:22 +0545

    alias also added

--- server/modules/categories/category.model.js
+++ server/modules/categories/category.model.js
@@ -1,9 +1,9 @@
 const { Schema, model } = require("mongoose");
+const commonSchema = require("../../utils/commonSchema");
 const CategorySchema = new Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
-  size: { type: String },
-  skus: [skuSchema],
+  slug: { type: String, required: true },
+  alias: [{ type: String }],
 });
 
 module.exports = model("Category", CategorySchema);
